e3641ebe4e63e175794101a491f1406aba7bff17
Esma Sherwani <esmasherr@gmail.com>
2020-11-19 05:23:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,25 +4,25 @@ end
 addition
 
 def subtraction(num1= 10, num2 = 5)
-puts (num1 - num2)
+return (num1 - num2)
 end
 subtraction
 
 def division(num1= 50 , num2= 2)
-puts (num1/num2)
+return (num1/num2)
 end
 division
 
 def multiplication(num1= 4, num2= 30)
-puts (num1 * num2)
+return (num1 * num2)
 end
 multiplication
 
 def modulo(num1= 34, num2= 5)
-puts (num1%num2)
+return (num1%num2)
 end
 modulo
 def square_root(num= Math.sqrt(81))
-  puts (num)
+  return (num)
 end
 square_root
